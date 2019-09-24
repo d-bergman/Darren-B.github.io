d8ca5dc68763491d595e5c3e2c16fb82ebe7a6bd
Bhumika-P <bhumika.9117@gmail.com>
2019-09-23 20:55:20 -0500

    Debugging

--- assets/js/tech-tree.js
+++ assets/js/tech-tree.js
@@ -95,4 +95,15 @@ function load_tree() {
         setup(jsonData);
         _load(jsonData);
     });
+    $.getJSON('anomalies.json', function(jsonData) {
+        // Event techs don't really need a Tree
+        $(jsonData).each(function(index, item) {
+            setup(item);
+            var e = $("<div>").html(item.innerHTML);
+            e.attr("class",item.HTMLclass)
+            e.addClass("node").addClass("tech").addClass("anomaly");
+            $('#tech-tree-anomalies').append(e);
+            init_tooltips();
+        });
+    });
 }
